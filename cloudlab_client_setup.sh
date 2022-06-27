b7d62c5b0600ef6c9baf3e59d9d6cb5c748712e9
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 14:43:54 -0400

    switch to correct branch in noise repo

--- cloudlab_client_setup.sh
+++ cloudlab_client_setup.sh
@@ -3,5 +3,6 @@ sudo apt update && sudo apt upgrade -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
 cd .. && git clone git@github.com:princeton-sns/noise.git %% cd noise
+git checkout remotes/origin/web-bench/latency
 cd libnoise-client/ && npm install && cd ..
 cd bench/web-bench/client && npm install
--- cloudlab_server_setup.sh
+++ cloudlab_server_setup.sh
@@ -3,6 +3,7 @@ sudo apt update && sudo apt upgrade -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
 cd .. && git clone git@github.com:princeton-sns/noise.git %% cd noise
+git checkout remotes/origin/web-bench/latency
 cd libnoise-server/ && npm install && cd ..
 cd bench/web-bench/server
 node index.js
