7e0de40d83f3a6ee44d09cf161a08cdf7977506d
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 15:23:02 -0400

    add sudo to script

--- cloudlab_client_setup.sh
+++ cloudlab_client_setup.sh
@@ -2,7 +2,7 @@
 #sudo apt update && sudo apt upgrade -y
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 #sudo apt install -y nodejs
-cd / && mkdir gaggle && cd gaggle && chmod 774 .
+cd / && sudo mkdir gaggle && cd gaggle && sudo chmod 774 .
 git clone git@github.com:princeton-sns/noise.git && cd noise
 git checkout remotes/origin/web-bench/latency
 #cd libnoise-client/ && npm install && cd ..
