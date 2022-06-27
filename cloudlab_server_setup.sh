2cc70f1c15dc41be47619a22eb49ac2eac4c1511
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 15:55:13 -0400

    test create new dir in /local

--- cloudlab_server_setup.sh
+++ cloudlab_server_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#sudo apt update && sudo apt upgrade -y
-#curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-#sudo apt install -y nodejs
-cd / && sudo mkdir gaggle && cd gaggle && sudo chmod 774 .
-git clone git@github.com:princeton-sns/noise.git && cd noise
-git checkout remotes/origin/web-bench/latency
+sudo apt update && sudo apt upgrade -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt install -y nodejs
+cd /local && mkdir gaggle && cd gaggle #&& sudo chmod 774 .
+#git clone git@github.com:princeton-sns/noise.git && cd noise
+#git checkout remotes/origin/web-bench/latency
 #cd libnoise-server/ && npm install && cd ..
 #cd bench/web-bench/server
