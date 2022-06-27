af2627bb9df9319e4332b4793132e667498f38a6
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 14:58:38 -0400

    fix typo

--- cloudlab_server_setup.sh
+++ cloudlab_server_setup.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
-cd .. && git clone git@github.com:princeton-sns/noise.git %% cd noise
+cd .. && git clone git@github.com:princeton-sns/noise.git && cd noise
 git checkout remotes/origin/web-bench/latency
 cd libnoise-server/ && npm install && cd ..
 cd bench/web-bench/server
