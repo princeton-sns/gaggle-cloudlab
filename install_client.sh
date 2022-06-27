ecf925fcf69e74e1efe21e948499c5ea9f405964
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 17:14:36 -0400

    create tmux instance from script

--- install_client.sh
+++ install_client.sh
@@ -3,3 +3,4 @@ git clone git@github.com:princeton-sns/noise.git && cd noise
 git checkout remotes/origin/web-bench/latency
 cd libnoise-client/ && npm install && cd ..
 cd bench/web-bench/client && npm install
+tmux new
--- install_server.sh
+++ install_server.sh
@@ -3,3 +3,4 @@ git clone git@github.com:princeton-sns/noise.git && cd noise
 git checkout remotes/origin/web-bench/latency
 cd libnoise-server/ && npm install && cd ..
 cd bench/web-bench/server
+tmux new
