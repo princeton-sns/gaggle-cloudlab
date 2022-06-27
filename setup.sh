4d387f702d9a767fa37249a99da7fcceb62662d3
Natalie Popescu <nataliepopescu@gmail.com>
2022-06-27 17:27:55 -0400

    merge setup scripts

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
 cd /local && mkdir gaggle && cd gaggle && sudo chmod 774 .
 cp /local/repository/install_server.sh .
+cp /local/repository/install_client.sh .
--- setup_client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-sudo apt install -y nodejs
-cd /local && mkdir gaggle && cd gaggle && sudo chmod 774 .
-cp /local/repository/install_client.sh .
